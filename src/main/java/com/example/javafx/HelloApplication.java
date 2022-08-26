9c79d759b15f0f7d0c167f4ac9ccaa7dc436ede2
paheter <paheter@gmail.com>
2022-08-27 00:50:40 +0700

    test

---
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -0,0 +1,36 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), stage.getMaxWidth(), stage.getMaxHeight());
+
+        Image image = new Image("C:\\Users\\Илья\\IdeaProjects\\javafx\\src\\main\\resources\\icons\\iconMain.png"); /// C:\Users\***\IdeaProjects\javafx\src\main\resources\icons\iconMain.png
+        stage.getIcons().add(image);
+
+        stage.setTitle("Привет!");
+        stage.setScene(scene);
+        stage.show();
+        scene.getCamera();
+        Button btn1 = new Button();
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/com/example/javafx/HelloController.java
@@ -0,0 +1,73 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Stage stage;
+    @FXML
+    private TextArea textAreaPole;
+    @FXML
+    private MenuItem btnNew;
+
+    File file;
+
+    @FXML
+    protected void onButtonClickNew() {
+
+        textAreaPole.setText("");
+
+    }
+
+    @FXML
+    protected void onClickOpenFile() {
+
+        FileChooser FileChooser = new FileChooser();                            // FileChooser диалоговое окно выбора файла
+        FileChooser.setTitle("Открыть файл ресурсов");
+        FileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("txt", "*.txt")
+
+        );
+
+        file = FileChooser.showOpenDialog(stage);                      // Помещаем выбраный файл шоб можно было работать с ним
+
+        Scanner obj = null;                                            // самый простой сканер
+        try {
+            obj = new Scanner(file);                                  // ипаное исключение на всякий,
+        } catch (FileNotFoundException e) {                           // испоьзуется showOpenDialog который тока текст файлы открывает
+            throw new RuntimeException(e);                           // смысл исключения, но ide показывает лучше поставить хз, пусть будет. Я нуб
+        }
+        while (obj.hasNextLine())
+            textAreaPole.setText(obj.nextLine());                   // записываем считаное в поле
+    }
+
+    @FXML
+    protected void onAboutClick() {
+
+        JFrame jFrame = new JFrame();
+        JOptionPane.showMessageDialog(jFrame, "paheterSorokDva");
+
+    }
+
+}
+
+
